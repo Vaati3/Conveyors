72a79a47f4f2035985fd0925da1a4e4bd682ea19
Honorin <honorin.pouteau@gmail.com>
2025-01-29 22:56:27 +0100

    place belts

--- Game/Belts/Belts.cs
+++ Game/Belts/Belts.cs
@@ -13,12 +13,14 @@ public enum Direction {
 
 public partial class Belt : Node2D
 {
+    public Vector2I pos {get; private set;}
 	public Direction direction {private set; get;}
-	protected AnimatedSprite2D sprite;
-    protected Queue<Item> items;
+	public AnimatedSprite2D sprite {get; private set;}
+    private Queue<Item> items;
 
-	public Belt(Direction direction)
+	public Belt(Vector2I pos, Direction direction, Belt previousBelt)
 	{
+        this.pos = pos;
         this.direction = direction;
         sprite = new AnimatedSprite2D(){
             SpriteFrames = GD.Load<SpriteFrames>("res://Game/Belts/BeltAnim.tres"),
@@ -26,6 +28,9 @@ public partial class Belt : Node2D
         };
         AddChild(sprite);
         sprite.Play(direction.ToString());
+        sprite.Animation = direction.ToString();
+        if (previousBelt != null)
+            sprite.SetFrameAndProgress(previousBelt.sprite.Frame, previousBelt.sprite.FrameProgress);
 	}
 
     public override void _Process(double delta)
@@ -36,4 +41,98 @@ public partial class Belt : Node2D
     // protected abstract void MoveItems();
 
     // public abstract void GiveItem(Item item);
+
+    public void ChangeDirection(Direction direction)
+    {
+        this.direction = direction;
+        int frame = sprite.Frame;
+        float frameProgress = sprite.FrameProgress;
+        sprite.Animation = direction.ToString();
+        sprite.SetFrameAndProgress(frame, frameProgress);
+    }
+
+    public static Direction GetBeltDirection(Vector2I pos, Vector2I dir, Belt previousBelt)
+	{
+		if (previousBelt == null)
+		{
+			if (dir.X != 0)
+        	{
+            	if (dir.X > 0)
+                	return Direction.LeftRight;
+                return Direction.RightLeft;
+        	}
+            if (dir.Y > 0)
+                return Direction.BottomTop; 
+            return Direction.TopBottom;
+		}
+		
+		switch (previousBelt.direction)
+		{
+			case Direction.TopBottom:
+				if (previousBelt.pos.X != pos.X)
+				{
+					if (previousBelt.pos.X > pos.X)
+					{
+						previousBelt.ChangeDirection(Direction.TopLeft);
+						return Direction.RightLeft;
+					}
+					previousBelt.ChangeDirection(Direction.TopRight);
+					return Direction.LeftRight;
+				}
+				if (previousBelt.pos.Y > pos.Y)
+				{
+					previousBelt.ChangeDirection(Direction.BottomTop);
+					return Direction.BottomTop;
+				}
+				return Direction.TopBottom;
+			case Direction.BottomTop:
+				if (previousBelt.pos.X != pos.X)
+				{
+					if (previousBelt.pos.X > pos.X)
+					{
+						previousBelt.ChangeDirection(Direction.BottomLeft);
+						return Direction.RightLeft;
+					}
+					previousBelt.ChangeDirection(Direction.BottomRight);
+					return Direction.LeftRight;
+				}
+				if (previousBelt.pos.Y > pos.Y)
+					return Direction.BottomTop;
+				previousBelt.ChangeDirection(Direction.TopBottom);
+				return Direction.TopBottom;
+			case Direction.LeftRight:
+				if (previousBelt.pos.Y != pos.Y)
+				{
+					if (previousBelt.pos.Y > pos.Y)
+					{
+						previousBelt.ChangeDirection(Direction.LeftTop);
+						return Direction.BottomTop;
+					}
+					previousBelt.ChangeDirection(Direction.LeftBottom);
+					return Direction.TopBottom;
+				}
+				if (previousBelt.pos.X > pos.X)
+				{
+					previousBelt.ChangeDirection(Direction.RightLeft);
+					return Direction.RightLeft;
+				}
+				return Direction.LeftRight;
+			case Direction.RightLeft:
+				if (previousBelt.pos.Y != pos.Y)
+				{
+					if (previousBelt.pos.Y > pos.Y)
+					{
+						previousBelt.ChangeDirection(Direction.RightTop);
+						return Direction.BottomTop;
+					}
+					previousBelt.ChangeDirection(Direction.RightBottom);
+					return Direction.TopBottom;
+				}
+				if (previousBelt.pos.X > pos.X)
+					return Direction.RightLeft;
+				previousBelt.ChangeDirection(Direction.LeftRight);
+				return Direction.LeftRight;
+		}
+		return 0;
+	}
 }
--- Game/Map.cs
+++ Game/Map.cs
@@ -1,15 +1,50 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Map : Node2D
 {
+	const int tilesize = 128;
+	Dictionary<Vector2I, Belt> belts;
+	Belt synchroBelt = null;
+	Belt previousBelt = null;
+
+	private void CreateBelt(Vector2I pos, Vector2I dir)
+	{	
+		if (!belts.ContainsKey(pos))
+		{
+			Belt belt = new Belt(pos, Belt.GetBeltDirection(pos, dir, previousBelt), synchroBelt);
+			belt.Position = pos * tilesize;
+			belts.Add(pos, belt);
+			AddChild(belt);
+			previousBelt = belt;
+			if (synchroBelt == null)
+				synchroBelt = belt;
+		}
+	}
+
 	public override void _Ready()
 	{
-		Belt belt = new Belt(Direction.LeftBottom);
-		belt.Position = new Vector2(64, 64);
-		Belt belt1 = new Belt(Direction.TopRight);
-		belt1.Position = new Vector2(64, 192);
-		AddChild(belt);
-		AddChild(belt1);
+		belts = new Dictionary<Vector2I, Belt>();
 	}
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion motion)
+		{
+			if (Input.IsActionPressed("Click"))
+			{
+				Vector2I pos = new Vector2I((int)motion.Position.X / tilesize, (int)motion.Position.Y / tilesize);
+				Vector2I dir = Vector2I.Zero; 
+				if (MathF.Abs(motion.Velocity.X) > MathF.Abs(motion.Velocity.Y))
+					dir.X = motion.Velocity.X > 0 ? 1 : -1;
+				else
+					dir.Y = motion.Velocity.Y > 0 ? 1 : -1;
+				
+				CreateBelt(pos, dir);
+			} else {
+				previousBelt = null;
+			}
+		}
+    }
 }
